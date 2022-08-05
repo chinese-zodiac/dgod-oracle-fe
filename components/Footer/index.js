df9e6bcaaecc11d6c4962ba0ab63466a32475a70
Plastic Digits <plasticdigits@protonmail.com>
2022-08-05 06:47:12 -0700

    Bump to v0.1.2

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -17,7 +17,7 @@ function Footer() {
         </a>
       </div>  
       <p className='pb-6'>
-        v0.1.1
+        v0.1.2
       </p>
       <p>
         AS WITH ANY BLOCKCHAIN PROJECT: <b>Do your own research before buying DGOD.</b><br/>
