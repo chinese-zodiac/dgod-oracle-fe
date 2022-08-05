7403d39ddf472632215d29ffacf5639f00328c9b
Plastic Digits <plasticdigits@protonmail.com>
2022-08-05 05:03:20 -0700

    Bump version

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -17,7 +17,7 @@ function Footer() {
         </a>
       </div>  
       <p className='pb-6'>
-        v0.1.0
+        v0.1.1
       </p>
       <p>
         AS WITH ANY BLOCKCHAIN PROJECT: <b>Do your own research before buying DGOD.</b><br/>
