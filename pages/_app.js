d65644036019170457c5a863bec37feaa6ba8d06
Plastic Digits <plasticdigits@protonmail.com>
2022-08-05 00:16:42 -0700

    Fix meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -44,17 +44,17 @@ class MyApp extends App {
               
             <meta property="og:title" content="DogeGod Oracle | Earn Dogecoin (DOGE) every second for life." />
             <meta property="og:site_name" content="DogeGod" />
-            <meta property="og:url" content="https://dogegod.io" />
+            <meta property="og:url" content="https://oracle.dogegod.io" />
             <meta property="og:description" content="#DogeGod, the greenest source of #Dogecoin on the planet. Come and join to the #GreenDoge revolution." />
             <meta property="og:type" content="article" />
-            <meta property="og:image" content={"https://sale.dogegod.io"+OpenGraphImg} />
+            <meta property="og:image" content={"https://oracle.dogegod.io"+OpenGraphImg} />
             <meta property="og:image:width" content="1200" /> 
             <meta property="og:image:height" content="630" />
 
             <meta name="twitter:card" content="summary_large_image"/>
             <meta name="twitter:site" content="https://dogegod.io" />
             <meta name="twitter:title" content="DogeGod Oracle | Earn Dogecoin (DOGE) every second for life."/>
-            <meta name="twitter:image" content={"https://sale.dogegod.io"+OpenGraphImg} />
+            <meta name="twitter:image" content={"https://oracle.dogegod.io"+OpenGraphImg} />
             <meta name="twitter:image:width" content="1200"/>
             <meta name="twitter:image:height" content="630"/>
             <meta name="twitter:description" content="#DogeGod, the greenest source of #Dogecoin on the planet. Come and join to the #GreenDoge revolution."/>
