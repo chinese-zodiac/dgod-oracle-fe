d429f27e237d11b35ca98ae0d306d33dc7f93039
Plastic Digits <plasticdigits@protonmail.com>
2022-08-07 01:49:45 -0700

    Hide metamask button when no injected detected

--- components/Web3ModalButton/index.tsx
+++ components/Web3ModalButton/index.tsx
@@ -9,11 +9,28 @@ import { parseEther, formatEther } from '@ethersproject/units'
 import MetamaskLogo from '../../public/static/assets/images/metamask.svg';
 import WalletConnectLogo from '../../public/static/assets/images/walletconnect.svg';
 
+const INJECTED_STATE = {
+  PENDING: 'PENDING',
+  ACTIVE: 'ACTIVE',
+  NONE: 'NONE'
+};
+
 const Web3ModalButton = ({className}) => {
   const { account, activate, deactivate, chainId } = useEthers();
 
+  const [injectedState,setInjectedState] = useState(INJECTED_STATE.PENDING);
+
   const [activateError, setActivateError] = useState('');
   const { error } = useEthers();
+
+  useEffect(()=>{
+    if(window?.ethereum?.networkVersion) {
+      setInjectedState(INJECTED_STATE.ACTIVE);
+    } else {
+      setInjectedState(INJECTED_STATE.NONE);
+    }
+  },[])
+
   useEffect(() => {
     if (error) {
       console.log(error);
@@ -57,20 +74,38 @@ const Web3ModalButton = ({className}) => {
   return (
     <div className={"field has-addons is-justify-content-center "+className}>
     {!account ? (<>
-    <p className="control">
-      <button title="Connect your browser wallet" className="button is-dark is-rounded is-large" style={{width:"6em", backgroundColor:"rgba(0,10,40,0.8)",border:"solid #126a85 2px"}} onClick={()=>activateProvider("injected")} >
-        <span className="icon is-small p-1" >
-          <img src={MetamaskLogo} />
-        </span>
-      </button>
-    </p>
-    <p className="control">
-      <button title="Connect with WalletConnect" className="button is-dark is-rounded is-large" style={{width:"6em", backgroundColor:"rgba(0,10,40,0.8)",border:"solid #126a85 2px"}} onClick={()=>activateProvider("walletconnect")}>
-        <span className="icon is-small">
-          <img src={WalletConnectLogo} />
-        </span>
-      </button>
-    </p>
+    {(injectedState == INJECTED_STATE.ACTIVE) && (<>
+      <p className="control">
+        <button title="Connect your browser wallet" className="button is-dark is-rounded is-large" style={{width:"6em", backgroundColor:"rgba(0,10,40,0.8)",border:"solid #126a85 2px"}} onClick={()=>activateProvider("injected")} >
+          <span className="icon is-small p-1" >
+            <img src={MetamaskLogo} />
+          </span>
+        </button>
+      </p>
+      <p className="control">
+        <button title="Connect with WalletConnect" className="button is-dark is-rounded is-large" style={{width:"6em", backgroundColor:"rgba(0,10,40,0.8)",border:"solid #126a85 2px"}} onClick={()=>activateProvider("walletconnect")}>
+          <span className="icon is-small">
+            <img src={WalletConnectLogo} />
+          </span>
+        </button>
+      </p>    
+    </>)}
+    {(injectedState == INJECTED_STATE.NONE) && (<>
+      <p className="control">
+        <button title="Connect with WalletConnect" className="button is-dark is-rounded is-large" style={{width:"12em", backgroundColor:"rgba(0,10,40,0.8)",border:"solid #126a85 2px"}} onClick={()=>activateProvider("walletconnect")}>
+          <span className="icon is-small">
+            <img src={WalletConnectLogo} />
+          </span>
+        </button>
+      </p>    
+    </>)}
+    {(injectedState == INJECTED_STATE.PENDING) && (<>
+      <p className="control">
+        <button title="Connect with WalletConnect" className="button is-dark is-rounded is-large" style={{width:"12em", backgroundColor:"rgba(0,10,40,0.8)",border:"solid #126a85 2px"}} onClick={()=>activateProvider("walletconnect")}>
+            <span className='is-size-6'>ORACLE: <span className='has-text-warning'>CONNECTING...</span></span>
+        </button>
+      </p>  
+    </>)}
     </>) : (<>
     <p className="control">
       <button title="Disconnect your wallet" className="button is-dark is-rounded is-large" style={{width:"12em", backgroundColor:"rgba(0,10,40,0.8)",border:"solid #126a85 2px"}} onClick={() => {console.log("deactivate"); deactivate();}}>
