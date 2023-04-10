fe6b5cab55b5171eda8e06e4d24e60cd1cad6232
Plastic Digits <plasticdigits@protonmail.com>
2023-04-09 18:46:49 -0700

    Bump version number

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { SOCIAL_TWITTER, SOCIAL_TELEGRAM, SOCIAL_GITHUB} from '../../constants/social';
+import { SOCIAL_GITHUB, SOCIAL_TELEGRAM, SOCIAL_TWITTER } from '../../constants/social';
 import CZPower from '../../public/static/assets/images/czpower.png';
 
 function Footer() {
-    return(<footer id="footer" className="footer pb-7 mt-5" style={{position:"relative"}}>
+  return (<footer id="footer" className="footer pb-7 mt-5" style={{ position: "relative" }}>
     <div className="content has-text-centered">
       <div>
         <a className="m-2" href={SOCIAL_TELEGRAM} target="_blank">
@@ -15,17 +15,17 @@ function Footer() {
         <a className="m-2" href={SOCIAL_GITHUB} target="_blank">
           <span className="icon"><i className="fa-brands fa-github"></i></span>
         </a>
-      </div>  
+      </div>
       <p className='pb-6'>
-        v0.1.18
+        v0.1.19
       </p>
       <p>
-        AS WITH ANY BLOCKCHAIN PROJECT: <b>Do your own research before using this website or buying DGOD.</b><br/>
-        Nothing on this site or on related channels should be considered a promise by anyone, including but not limited to the developers and promoters of this site, to perform work to generate profits for anyone including but not limited to the following: the users of this site; DogeGod community members; CZodiac community members; DGOD holders; or anyone using any of the sites, smart contracts, social media channels, and any other media or tech related to DGOD, DogeCoin, and CZodiac or any of the community members. 
-        Dogegod.io, DGOD, CZodiac, and related technologies plus media are all experimental and must be used according to your personal financial situation and risk profile. 
-        <br/><br/>There are no guarantees of profits, but the smart contracts are guaranteed to deliver Dogecoin to DGOD holders as written on the BSC blockchain.
+        AS WITH ANY BLOCKCHAIN PROJECT: <b>Do your own research before using this website or buying DGOD.</b><br />
+        Nothing on this site or on related channels should be considered a promise by anyone, including but not limited to the developers and promoters of this site, to perform work to generate profits for anyone including but not limited to the following: the users of this site; DogeGod community members; CZodiac community members; DGOD holders; or anyone using any of the sites, smart contracts, social media channels, and any other media or tech related to DGOD, DogeCoin, and CZodiac or any of the community members.
+        Dogegod.io, DGOD, CZodiac, and related technologies plus media are all experimental and must be used according to your personal financial situation and risk profile.
+        <br /><br />There are no guarantees of profits, but the smart contracts are guaranteed to deliver Dogecoin to DGOD holders as written on the BSC blockchain.
       </p>
-      <a href="https://czodiac.com"><img src={CZPower} alt="Powered by CZodiac" style={{maxWidth:"360px"}} /></a> 
+      <a href="https://czodiac.com"><img src={CZPower} alt="Powered by CZodiac" style={{ maxWidth: "360px" }} /></a>
     </div>
   </footer>);
 }
