31a78e17aa176de4afb3861b9b36046dcaaf5765
Plastic Digits <plasticdigits@protonmail.com>
2023-04-14 07:10:06 -0700

    Update footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SOCIAL_GITHUB, SOCIAL_TELEGRAM, SOCIAL_TWITTER } from '../../constants/social';
+import { LINK_PRIVACY_POLICY, LINK_TERMS_OF_USE, SOCIAL_GITHUB, SOCIAL_TELEGRAM, SOCIAL_TWITTER } from '../../constants/social';
 import CZPower from '../../public/static/assets/images/czpower.png';
 
 function Footer() {
@@ -20,14 +20,16 @@ function Footer() {
         v0.1.19
       </p>
       <p>
-        AS WITH ANY BLOCKCHAIN PROJECT: <b>Do your own research before using this website or buying DGOD.</b><br />
-        Nothing on this site or on related channels should be considered a promise by anyone, including but not limited to the developers and promoters of this site, to perform work to generate profits for anyone including but not limited to the following: the users of this site; DogeGod community members; CZodiac community members; DGOD holders; or anyone using any of the sites, smart contracts, social media channels, and any other media or tech related to DGOD, DogeCoin, and CZodiac or any of the community members.
-        Dogegod.io, DGOD, CZodiac, and related technologies plus media are all experimental and must be used according to your personal financial situation and risk profile.
-        <br /><br />There are no guarantees of profits, but the smart contracts are guaranteed to deliver Dogecoin to DGOD holders as written on the BSC blockchain.
+        <a style={{ color: 'cyan' }} href={LINK_TERMS_OF_USE}>Terms of Use</a>
       </p>
-      <a href="https://czodiac.com"><img src={CZPower} alt="Powered by CZodiac" style={{ maxWidth: "360px" }} /></a>
+      <p>
+        <a style={{ color: 'cyan' }} href={LINK_PRIVACY_POLICY}>
+          Privacy Policy
+        </a>
+      </p>
+      <a href="https://czodiac.com"><img src={CZPower} alt="Powered by CZodiac" style={{ maxWidth: "180px", marginTop: '100px' }} /></a>
     </div>
-  </footer>);
+  </footer >);
 }
 
 export default Footer;
